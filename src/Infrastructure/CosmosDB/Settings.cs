e86181a06f40ef9c9af143ecb841330dc96f02da
Juniortru21 <139279189+Juniortru21@users.noreply.github.com>
2024-06-08 10:22:05 -0500

    Testa

--- src/Infrastructure/CosmosDB/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Infrastructure.CosmosDB;
-public class Settings
-{
-    public required string Uri { get; set; }
-    public required string Key { get; set; }
-    public required string DatabaseName {  get; set; }
-    public required string Container {  get; set; }
-}
